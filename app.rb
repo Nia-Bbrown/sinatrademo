8174989fafdafea07b0f2d305a8209c346fb1bec
Nia Brown <nbrown@missiondata.com>
2023-06-02 11:48:23 -0400

    Printing a list of cakes

--- app.rb
+++ app.rb
@@ -1,5 +1,13 @@
 require 'sinatra'
+require "erb"
 
+# next step will be to modify the code to display the whole list of cakes found in the cake.list file
+
+# Open cake.list file and ready each line
 get '/' do
-  "Hello, world"
+  file = File.open("cake.list")
+  @cakes = file.readlines.map(&:chomp)
+  erb :index
+  # File.foreach("cake.list") { |cake| puts cake }
 end
+
