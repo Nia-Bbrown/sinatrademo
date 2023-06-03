5c02c46ee1811c43adb97d26d701d0cc59d4533f
Nia Brown <nbrown@missiondata.com>
2023-06-02 22:11:33 -0400

    Slight Changes

--- app.rb
+++ app.rb
@@ -1,13 +1,28 @@
 require 'sinatra'
-require "erb"
+require 'json'
+# define a route that uses the helper for json
+# get '/cakes.json' do
+#   json :foo => 'bar'
+# end
+
+get '/cakes.json' do
+  content_type :json
+  cakes = File.readlines("cake.list").map(&:chomp)
+  cakes.to_json
+end
+
+get '/' do
+  erb :index
+end
 
 # next step will be to modify the code to display the whole list of cakes found in the cake.list file
 
-# Open cake.list file and ready each line
-get '/' do
-  file = File.open("cake.list")
-  @cakes = file.readlines.map(&:chomp)
-  erb :index
-  # File.foreach("cake.list") { |cake| puts cake }
-end
+# --------RUBY--------
+# Open cake.list file and ready each line --RUBY--
+# get '/' do
+#   file = File.open("cake.list")
+#   @cakes = file.readlines.map(&:chomp)
+#   erb :index
+#   # File.foreach("cake.list") { |cake| puts cake }
+# end
 
